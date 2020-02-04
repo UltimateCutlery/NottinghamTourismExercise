8677a15fd9cd55861523ce5a62b8d096516d3899
UltimateCutlery <57415618+UltimateCutlery@users.noreply.github.com>
2020-02-04 13:38:28 +0000

    ratings and external links

--- scripts/functions.js
+++ scripts/functions.js
@@ -13,3 +13,43 @@ function openP3() {
 function openHome() {
   location.href = "homepage.html";
 }
+
+function openA1(){
+  window.open("https://www.wollatonhall.org.uk/","_blank")
+}
+
+function openA2(){
+  window.open("https://www.nationaljusticemuseum.org.uk/","_blank")
+}
+
+function openA3(){
+  window.open("https://www.nationaljusticemuseum.org.uk/venue/city-of-caves/","_blank")
+}
+
+function openA4(){
+  window.open("https://www.visit-nottinghamshire.co.uk/things-to-do/nottingham-arboretum-p641471","_blank")
+}
+
+function openA5(){
+  window.open("https://trch.co.uk/","_blank")
+}
+
+function openA6(){
+  window.open("https://www.visit-nottinghamshire.co.uk/things-to-do/old-market-square-p593561","_blank")
+}
+
+function openA7(){
+  window.open("https://www.greensmill.org.uk/","_blank")
+}
+
+function openA8(){
+  window.open("https://www.nottinghamindustrialmuseum.org.uk/","_blank")
+}
+
+function openA9(){
+  window.open("http://www.stonebridgecityfarm.com/","_blank")
+}
+
+function openA10(){
+  window.open("https://www.thehauntedmuseum.co.uk/","_blank")
+}
